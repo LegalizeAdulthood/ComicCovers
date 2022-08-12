23f9f7bf671ee7a67f7397435c3521db42ff0207
Richard <legalize@xmission.com>
2022-08-12 13:56:03 -0600

    Extract URLs for all comics in a collection

--- app.js
+++ app.js
@@ -3,15 +3,38 @@
 const cheerio = require('cheerio');
 const request = require('request');
 
-request('https://my.comics.org/collection/33548/', function(err, res, html) {
+var page = 1;
+var collection = {"name": "", "covers": []};
+function processPage(html)
+{
     const $ = cheerio.load(html);
-    var collectionName = /Collection details: *(.*[^\s])\s+\//.exec($('h1').text())[1];
+    if (collection.name.length === 0)
+        collection.name = /Collection details: *(.*[^\s])\s+\//.exec($('h1').text())[1];
     $('div .thumbnail').each(function(index, elem) {
         var url = $(this).find('img')[0].attribs['src'];
         url = /^(.*)\?/.exec(url)[1].replace('/w100', '/w400');
         var pieces = /^(.*[^\s])\s\(.*\) #([0-9]+)/.exec($(this).find('div .caption').text());
         var name = (pieces[1] + ' #' + pieces[2]).replace(/ /g, '');
-        console.log('url=' + url);
-        console.log('name=' + name);
+        collection.covers.push([name, url]);
     });
+    var pageNum = parseInt($($('ul.pagination li.active')[0]).text());
+    return page++ === pageNum;
+}
+
+function fetchPage(page, next)
+{
+    request('https://my.comics.org/collection/33548/?page=' + page, function(err, res, html) {
+        if (err) {
+            next(err);
+            return;
+        }
+        if (processPage(html))
+            fetchPage(++page, next);
+        else
+            next();
+    });
+}
+
+fetchPage(page, function() {
+    console.log(JSON.stringify(collection, null, 4));
 });

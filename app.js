0ae3360f103f5f28deb9a9618f987f5aa2350e31
Richard <legalize@xmission.com>
2022-08-12 13:32:06 -0600

    Extract collection name

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const request = require('request');
 
 request('https://my.comics.org/collection/33548/', function(err, res, html) {
     const $ = cheerio.load(html);
+    var collectionName = /Collection details: *(.*[^\s])\s+\//.exec($('h1').text())[1];
     $('div .thumbnail').each(function(index, elem) {
         var url = $(this).find('img')[0].attribs['src'];
         url = /^(.*)\?/.exec(url)[1].replace('/w100', '/w400');

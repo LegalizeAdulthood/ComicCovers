5284bc8e69f7a4d4aa54d9c86bb6206c9ee36079
Richard <legalize@xmission.com>
2022-10-17 20:08:37 -0600

    Process a group of URLs, skip any covers already downloaded

--- app.js
+++ app.js
@@ -4,12 +4,45 @@ const async = require('async');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const path = require('path');
+const process = require('process');
 const request = require('request');
 const _ = require('underscore');
 
-var page = 1;
-var collection = { "url": 'https://my.comics.org/collection/33548/', "name": '', "covers": [] };
-function processPage(html)
+function fetchCover(collection, cover, next)
+{
+    var url = cover[1];
+    var filename = path.join(collection.name, cover[0] + '.jpg');
+    fs.access(filename, fs.constants.F_OK, function(err) {
+        if (err) {
+            // file does not (yet) exist
+            console.log('Fetch ' + url + ' as ' + filename);
+            request(url, next).pipe(fs.createWriteStream(filename));
+        } else {
+            console.log('Skipping ' + filename);
+            next();
+        }
+    });
+}
+
+function processCollection(collection, next)
+{
+    if (collection.name === '') {
+        console.log(JSON.stringify(collection, null, 4));
+        next(collection);
+        return;
+    }
+    fs.mkdir(collection.name, function(err) {
+        if (err && err.code !== 'EEXIST') {
+            console.log(collection.name + ' mkdir: ' + err);
+            next(err);
+            return;
+        }
+
+        async.eachLimit(collection.covers, 5, function(cover, nextCover) { fetchCover(collection, cover, nextCover); }, next);
+    });
+}
+
+function processPage(collection, page, html)
 {
     const $ = cheerio.load(html);
     var pageNum = parseInt($($('ul.pagination li.active')[0]).text());
@@ -20,13 +53,15 @@ function processPage(html)
         collection.name = /Collection details: *(.*[^\s])\s+\//.exec($('h1').text())[1].replace(/[?:]/g, '');
         console.log('Collection: ' + collection.name);
     }
-    console.log('Page ' + page);
+    console.log(collection.name + ' Page ' + page);
     $('div .thumbnail').each(function(index, elem) {
         var url = $(this).find('img')[0].attribs['src'];
         if (/noupload/.test(url))
             return;
-           
-        url = /^(.*)\?/.exec(url)[1].replace('/w100', '/w400');
+
+        url = /nocover/.test(url) ?
+            'https://files1.comics.org/static/img/nocover_large.png' :
+            /^(.*)\?/.exec(url)[1].replace('/w100', '/w400');
         var issueName = $(this).find('div .caption').text();
         var pieces = /^(.*[^\s])\s\(.* ([0-9]+) series\) #([0-9]+)/.exec(issueName);
         var name;
@@ -43,44 +78,45 @@ function processPage(html)
     return true;
 }
 
-function fetchPage(page, next)
+function fetchPage(collection, page, next)
 {
     request(collection.url + '?page=' + page, function(err, res, html) {
         if (err) {
             next(err);
             return;
         }
-        if (processPage(html))
-            fetchPage(++page, next);
+        if (processPage(collection, page, html))
+            fetchPage(collection, ++page, next);
         else
             next();
     });
 }
 
-function fetchCover(cover, next)
+function fetchCollection(url, next)
 {
-    var url = cover[1];
-    var filename = path.join(collection.name, cover[0] + '.jpg');
-    console.log('Fetch ' + url + ' as ' + filename);
-    request(url, next).pipe(fs.createWriteStream(filename));
-}
+    var page = 1;
+    var collection = { url: url, name: '', covers: [] };
+    fetchPage(collection, page, function(err) {
+        if (err) { next(err); return; }
 
-function processCollection()
-{
-    fs.mkdir(collection.name, function() {
-        async.eachLimit(collection.covers, 5, fetchCover, function() {
-            console.log('Done');
-        });
+        processCollection(collection, next);
     });
 }
 
 function main()
 {
-    if (process.argv.length === 3) {
-        collection.url = process.argv[2];
+    if (process.argv.length !== 3) {
+        console.log("node app.js <url-file>");
+        process.exit(1);
     }
-    fetchPage(page, function() {
-        processCollection();
+
+    var urlFile = process.argv[2];
+    fs.readFile(urlFile, { encoding: 'utf8' }, function(err, data) {
+        if (err) { console.log("Err: " + err); return err; }
+
+        data = data.trim().replace(/\r/g, '');
+        var urls = data.split("\n");
+        async.eachLimit(urls, 10, fetchCollection);
     });
 }
 
